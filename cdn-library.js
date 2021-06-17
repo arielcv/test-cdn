c904b1e87dc6b2f57e710c43ec04e7929ec4e90f
arielcv <arielcv96@nauta.cu>
2021-06-17 18:05:21 -0400

    update the cdn-library

--- cdn-library.js
+++ cdn-library.js
@@ -1,15 +1,29 @@
-const logoBlanco = "https://cdn.jsdelivr.net/gh/arielcv/test-cdn/logo blanco 2.svg"
-const logoAzul = "https://cdn.jsdelivr.net/gh/arielcv/test-cdn/logo azul 1.svg"
+const logoBlanco = "https://cdn.jsdelivr.net/gh/arielcv/test-cdn/logo-blanco-2.svg"
+const logoAzul = "https://cdn.jsdelivr.net/gh/arielcv/test-cdn/logo-azul-1.svg"
 
-const buttonCreator = (btnFunction, src) => `<button class="gpbutton" onclick="${btnFunction}">
-<img class="g-logo-button" src=${src}>
-<div id='x'">
+/**
+ * Generates a graviti button pay
+ * @param {string} src - The source of the button image
+ * @param {string} product - The id of the product related to button
+ * @param {string} price - The price of the product related to button
+ */
+const buttonCreator = (src, product, price) => (
+    `<button class="gpbutton" onclick="console.log('${product}', '${price}')">
+        <img class="g-logo-button" src=${src}>
+        <div id='x'>
             Paga con Graviti
         </div>
     </button>`
+)
 
+/**
+ * Authenticate your website
+ */
 function authenticate(){
-    console.log('Authenticated')
+    fetch("https://api.agify.io/?name=michael",{
+        method:'GET',
+    })
+    .then( response => response.json().then(({name}) => console.log(name)) )
 }
 
 function send(id_product){
@@ -21,16 +35,20 @@ function sum(product, price){
 }
 
 function buttonCreation(id, productId){
-    buttonFunction = () => sum(id, productId)
-    const buttonHTML = buttonCreator(buttonFunction, logoBlanco)
+    const buttonHTML = buttonCreator(logoBlanco, id, productId)
     const button = document.createElement('div')
     button.innerHTML = buttonHTML
+    buttonFunction = () => sum(id, productId)
     return button
 }
 
 const element = document.querySelectorAll('.product')
 
+
+
 Object.keys(element).map(e => {
     el = element[e]
     el.appendChild(buttonCreation(el.dataset.product, el.dataset.price))
+    console.log("🚀 ~ file: cdn-library.js ~ line 38 ~ Object.keys ~ el", el)
 })
+authenticate()
