18d6ec3549623c7a7c0a8b7fda45002dff7e77e9
arielcv <arielcv96@nauta.cu>
2021-06-17 16:03:21 -0400

    add some changes

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# test-cdn
+# Welcome to GRAVITI PAGOS CDN:
 
-Welcome to GRAVITI PAGOS CDN:
+*At Graviti we want your products reach the people who need them most.
+With our integration of **Graviti Pagos**, you will be able to offer credit for your products to low-income, unbanked users who cannot pay cash and do not have a credit card. All this with zero risk for you. Since we pay you in cash, in a single exhibition, without complications.
+Increase your sales, increase your conversion and reach new markets by offering credit for your products with Graviti Pagos.*
 
 We provided with:
-- The styles to the Paga con Graviti button
-- Our logo, in order to introduce it where you see it better
+- The styles to the **Paga con Graviti** button
+- Our logo (in blue and white colors), in order to allow you introduce it where you see it better
 - Some functions to make you easy and less code the integration process
--- cdn-library.js
+++ cdn-library.js
@@ -1,3 +1,13 @@
+const logoBlanco = "https://cdn.jsdelivr.net/gh/arielcv/test-cdn/logo blanco 2.svg"
+const logoAzul = "https://cdn.jsdelivr.net/gh/arielcv/test-cdn/logo azul 1.svg"
+
+const buttonCreator = (btnFunction, src) => `<button class="gpbutton" onclick="${btnFunction}">
+<img class="g-logo-button" src=${src}>
+<div id='x'">
+            Paga con Graviti
+        </div>
+    </button>`
+
 function authenticate(){
     console.log('Authenticated')
 }
@@ -5,3 +15,22 @@ function authenticate(){
 function send(id_product){
     console.log(`Sending ${id_product}`)
 }
+
+function sum(product, price){
+    console.log(`The price of ${product} is ${price}`)
+}
+
+function buttonCreation(id, productId){
+    buttonFunction = () => sum(id, productId)
+    const buttonHTML = buttonCreator(buttonFunction, logoBlanco)
+    const button = document.createElement('div')
+    button.innerHTML = buttonHTML
+    return button
+}
+
+const element = document.querySelectorAll('.product')
+
+Object.keys(element).map(e => {
+    el = element[e]
+    el.appendChild(buttonCreation(el.dataset.product, el.dataset.price))
+})
